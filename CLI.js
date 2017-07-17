c7075ebe64cc5c45295bf4cb361859a2d3a0f648
cadie <cadie@dreamfactoryagency.com>
2017-07-17 00:03:09 -0400

    add questions file and remove them from the CLI

--- CLI.js
+++ CLI.js
@@ -1,11 +1,9 @@
 var inquirer = require("inquirer");
 var BasicCard = require("./BasicCard.js");
 var ClozeCard = require("./ClozeCard.js");
+var BasicCards = require("./Questions.js");
+var ClozeCards = require("./Questions.js");
 
-var basicCard1 = new BasicCard("What does HTML stand for?", "Hyper Text Markup Language");
-var basicCard2 = new BasicCard("What does CSS stand for?", "Cascading Style Sheet");
-var basicCard3 = new BasicCard("Inside which HTML element do we put JavaScript code?", "<javascript>");
 
-var clozeCard1 = new ClozeCard("jQuery is a Javascript Library", "Javascript");
-var clozeCard2 = new ClozeCard("An object method is an object property containing a function", "function");
-var clozeCard3 = new ClozeCard("A boolean data type represents one of two values: true or false", "boolean");
+console.log(BasicCards[0]);
+console.log(ClozeCards[2]);
---
+++ Questions.js
@@ -0,0 +1,34 @@
+var BasicCards = [
+	{
+		front: "What does HTML stand for?",
+		back: "Hyper Text Markup Language"
+	},
+	{
+		front: "What does CSS stand for?",
+		back: "Cascading Style Sheet"
+	},
+	{
+		front: "Inside which HTML element do we put JavaScript code?",
+		back: "<javascript>"
+	}
+]
+
+var ClozeCards = [
+	{
+		fullText: "jQuery is a Javascript Library",
+		cloze: "Javascript"
+	},
+	{
+		fullText: "An object method is an object property containing a function",
+		cloze: "function"
+	},
+	{
+		fullText: "A boolean data type represents one of two values: true or false",
+		cloze: "boolean"
+	}
+]
+
+
+
+module.exports = BasicCards;
+module.exports = ClozeCards;
