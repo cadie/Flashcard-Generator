5423d0e57b2cbe0b894107e038a94969b6df9c5f
cadie <cadie@dreamfactoryagency.com>
2017-07-17 00:27:27 -0400

    start of the inquirer code

--- CLI.js
+++ CLI.js
@@ -5,5 +5,43 @@ var BasicCards = require("./Questions.js");
 var ClozeCards = require("./Questions.js");
 
 
-console.log(BasicCards[0]);
-console.log(ClozeCards[2]);
+// Variable that holds the cloze-deleted questions list
+// var closeQuestions = [];
+
+// Populate the cloze-deleted questions list
+// for (var i = 0; i < questions.length; i++) {
+// 	var q = new flashCards.ClozeCard(questions[i].full, questions[i].cloze);
+// 	closeQuestions.push(q);
+// }
+
+// What question the user is currently on
+var currentQuestion = 0;
+
+var initialPrompt = {
+    type: 'list',
+    message: 'What kind of cards do you want to use?',
+    choices: [
+      'Basic Flashcards',
+      'Cloze Flashcards'
+    ],
+    name: 'initialpromptChoice'
+    };
+
+
+function app() {
+  inquirer.prompt(initialPrompt).then(function(answers) {
+    if (answers.initialpromptChoice === 'Basic Flashcards') {
+      console.log('Basic was chosen');
+      basicCardQuestions();
+    } else {
+      console.log('Cloze was chosen');
+    }
+  });
+}
+
+function basicCardQuestions() {
+  //this function asks the cloze card questions
+}
+
+// Begin asking the questions!
+app();
